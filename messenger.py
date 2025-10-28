8affb53455c0b85ce15456066655433a585a617d
Leonard Joubert des Ouches <leonard.joubert_des_ouches@etu.minesparis.psl.eu>
2025-10-28 16:53:36 +0100

    les fonctions ont été séparées, mais la fonction channel ne marche pas bien

--- messenger.py
+++ messenger.py
@@ -18,7 +18,7 @@ server = {
         }
     ]
 }
-def messenger():
+def menu_principal():
     print('=== Messenger ===')
     print('x. Leave')
     print('u. utilisateurs')
@@ -29,53 +29,74 @@ def messenger():
         print('Bye!')
 
     elif choice == 'u':
-        print("===User list===")
-        for user in server['users']:
-            print(user['id'], user['name'])
-        print('------------------')
-        print("a :  ajouter un utilisateur")
-        print("r :  revenir au menu principal")
-        choice2 = input('Select an option')
-
-        if choice2 == 'a':
-            newid = int(input("new user id?"))
-            newname = input("new user name?")
-            server['users'].append({'id': newid, 'name': newname})
-            messenger()
-
-        if choice2 == 'r':
-            messenger()
-
+        utilisateurs()
+ 
     elif choice == 'c':
-        print("===Channel list===")
-        for channel in server['channels']:
-            print('id:',channel['id'],'| name:', channel['name'])
-        print('------------------')
-        print("m :  lire les messages")
-        print("a :  ajouter un channel")
-        print("r :  revenir au menu principal")
-        choice3 = input('Select an option')
+        channels()
         
-        if choice3=='m':
-            groupid = int(input("id du groupe:"))
-            for mess in server['messages']: 
-                if mess['channel'] == groupid:
-                    sender_id = mess['sender_id']
-                    for user in server['users']:
-                        if sender_id == user['id']:
-                            sender = user['name']
-                    print(sender,':', mess['content'])
-                else:
-                    print(groupid, "Ce channel n'existe pas")
-
-        elif choice3 == 'a':
-            print('pas encore codé')
-
-        elif choice3=='r':
-            messenger()
-
 
     else:
         print('Unknown option')
 
-messenger()
+
+def utilisateurs():
+    print("===User list===")
+    for user in server['users']:
+        print(user['id'], user['name'])
+    print('------------------')
+    print("a :  ajouter un utilisateur")
+    print("r :  revenir au menu principal")
+    choice2 = input('Select an option: ')
+
+    if choice2 == 'a':
+        newid = int(input("new user id?"))
+        newname = input("new user name?")
+        server['users'].append({'id': newid, 'name': newname})
+        utilisateurs()
+
+    if choice2 == 'r':
+        menu_principal()
+
+    else:
+        print('Unknown option')
+        utilisateurs()
+
+def channels():
+    print("===Channel list===")
+    for channel in server['channels']:
+        print('id:',channel['id'],'| name:', channel['name'])
+    print('------------------')
+    print("m :  lire les messages")
+    print("a :  ajouter un channel")
+    print("r :  revenir au menu principal")
+    print('x :  leave')
+    choice3 = input('Select an option: ')
+    
+    if choice3 == 'x':
+        print('Bye!')
+
+    elif choice3=='m':
+        groupid = int(input("id du groupe:"))
+        for mess in server['messages']: 
+            if mess['channel'] == groupid:
+                sender_id = mess['sender_id']
+                for user in server['users']:
+                    if sender_id == user['id']:
+                        sender = user['name']
+                print(sender,':', mess['content'])
+        channels()
+
+    elif choice3 == 'a':
+        print('pas encore codé ;)')
+
+    elif choice3=='r':
+        menu_principal()
+
+    else:
+        print('Unknown option')
+        channels()
+
+
+
+
+menu_principal()
